d0996f129257ac243de27271c1aa9187a14ef902
VloneDSb <kozachekivan60@gmail.com>
2023-06-05 21:39:27 +0300

    update

--- 05_06_2023.py
+++ 05_06_2023.py
@@ -5,22 +5,8 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.properties import ObjectProperty
 from kivy.uix.textinput import TextInput
-from kivy.app import App
-from kivy.lang import Builder
 from kivy.properties import StringProperty
-from kivy.uix.boxlayout import BoxLayout
 
-
-# Window.clearcolor = (.9, .9, .9, 1)
-# class Main(App):
-#     def build(self):
-#         lable = Label(text='Hello мир, манера крутит мир')
-#         return lable
-    
-
-# if __name__ == '__main__':
-#     app = Main()
-#     app.run()
 class MainWidget(BoxLayout):
     oplata_label = ObjectProperty()
     name_input = ObjectProperty()
@@ -36,17 +22,7 @@ class MainWidget(BoxLayout):
 class Main(App):
     def build(self):
         return MainWidget()
-    
 
-        # layout = BoxLayout()
-        # lable1 = Label(text='Hello мир ')
-        # lable2 = Label(text='!')
-        # lable3 = Label(text='Mанера крутит мир')
-        # layout.add_widget(lable1)
-        # layout.add_widget(lable2)
-        # layout.add_widget(lable3)
-        # return layout
-    
 
 if __name__ == '__main__':
     app = Main()
