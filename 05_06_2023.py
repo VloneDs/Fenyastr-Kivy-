bd6cfd2aa0cf76035e241967b4d5a54eec9c16a8
VloneDSb <kozachekivan60@gmail.com>
2023-06-05 21:31:31 +0300

    update !!

--- 05_06_2023.py
+++ 05_06_2023.py
@@ -5,6 +5,12 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.properties import ObjectProperty
 from kivy.uix.textinput import TextInput
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+
+
 # Window.clearcolor = (.9, .9, .9, 1)
 # class Main(App):
 #     def build(self):
@@ -18,10 +24,15 @@ from kivy.uix.textinput import TextInput
 class MainWidget(BoxLayout):
     oplata_label = ObjectProperty()
     name_input = ObjectProperty()
+    image_source = StringProperty()
 
     def oplata(self):
         self.oplata_label.text = "Феня ушел в тильт (вас заскамили)"
+        self.image_source = 'da.png'
 
+    def change_image(self):
+        app.root.ids['my_image'].source = 'da.png'
+    
 class Main(App):
     def build(self):
         return MainWidget()
